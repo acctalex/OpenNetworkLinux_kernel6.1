cfe41e6e3da8d92ab70520658478f33cb59d1ddc
Alex Lai <alex_lai@accton.com>
2024-10-11 01:12:20 +0000

    [Accton][as7326-56x] Fix the PSU bug
    1. Only let known 3Y PSUs read the sysfs, psu_mfr_model_opt, by using
       the pmbus command, 0xD0.
    
    Signed-off-by: Alex Lai <alex_lai@accton.com>

--- packages/base/any/kernels/modules/ym2651y.c
+++ packages/base/any/kernels/modules/ym2651y.c
@@ -865,23 +865,30 @@ static struct ym2651y_data *ym2651y_update_device(struct device *dev)
                 goto exit;
             }
 
-            /* Read mfr_model_opt */
-            command = 0xd0;
-            length  = 1;
+            if ((strncmp((data->mfr_model+1), "YM-2851J", strlen("YM-2851J")) == 0)||
+                (strncmp((data->mfr_model+1), "YM-2651Y", strlen("YM-2651Y")) == 0)||
+                (strncmp((data->mfr_model+1), "YPEB1200AM", strlen("YPEB1200AM")) == 0)) {
+            
+                /* Read mfr_model_opt */
+                command = 0xd0;
+                length  = 1;
 
-            /* Read first byte to determine the length of data */
-            status = ym2651y_read_block(client, command, &buf, length);
-            if (status < 0) {
-                dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
-                goto exit;
-            }
+                /* Read first byte to determine the length of data */
+                status = ym2651y_read_block(client, command, &buf, length);
+                if (status < 0) {
+                    dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+                    goto exit;
+                }
 
-            status = ym2651y_read_block(client, command, data->mfr_model_opt, buf+1);
-            data->mfr_model_opt[buf+1] = '\0';
+                status = ym2651y_read_block(
+                            client, command, data->mfr_model_opt, buf+1);
+                
+                data->mfr_model_opt[buf+1] = '\0';
 
-            if (status < 0) {
-                dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
-                goto exit;
+                if (status < 0) {
+                    dev_dbg(&client->dev, "reg %d, err %d\n", command, status);
+                    goto exit;
+                }
             }
 
             /* Read mfr_revsion */
