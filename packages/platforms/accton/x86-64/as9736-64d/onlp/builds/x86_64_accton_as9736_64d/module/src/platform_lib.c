e2a7e32640c724e435f43da0916f83e6003c758f
Alex Lai <alex_lai@accton.com>
2025-03-13 03:34:26 +0000

    [Accton][as9736-64d] Add new platform
    CPU : Broadwell-DE D1548
    DDR4 SDRAM : 16GB x 2 with ECC (SO-DIMM) DDR4 NOR Flash (Boot) : 16MB
    NOR Flash (Backup) : 16MB on SCM
    SSD : 128G NVMe SSD / 128G SATA SSD
    64 x QSFP-DD ports
    2 x SFP+ ports
    Power Supply: 12V/2400W PSU front to back airflow(AC to DC), 1+1 redundant load-sharing, hot-swappable
    Cooling: 4 FAN-tray modules with 4 pcs of 80mm x 80mm x 80mm 12V hot-swappable fans.
    Dimension: 2U , 440mm(W) x 649.2mm(L) x 87mm(H)
    
    Signed-off-by: Alex Lai <alex_lai@accton.com>

---
+++ packages/platforms/accton/x86-64/as9736-64d/onlp/builds/x86_64_accton_as9736_64d/module/src/platform_lib.c
@@ -0,0 +1,124 @@
+#include <onlp/onlp.h>
+#include <onlplib/file.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "platform_lib.h"
+#include <onlp/platformi/sfpi.h>
+#include "x86_64_accton_as9736_64d_log.h"
+
+#define I2C_PSU_MODEL_NAME_LEN 15
+#define I2C_PSU_FAN_DIR_LEN    3
+
+#define AIM_FREE_IF_PTR(p) \
+do \
+{ \
+	if (p) { \
+        	aim_free(p); \
+        	p = NULL; \
+	} \
+} while (0)
+
+psu_type_t get_psu_type(int id, char* modelname, int modelname_len)
+{
+	int ret = 0;
+	char *node = NULL;
+	char *mn = NULL;
+
+	/* Check AC model name */
+	node = (id == PSU1_ID) ? PSU1_AC_PMBUS_NODE(psu_mfr_model)
+			       : PSU2_AC_PMBUS_NODE(psu_mfr_model);
+
+	ret = onlp_file_read_str(&mn, node);
+
+	if (ret <= 0 || ret > I2C_PSU_MODEL_NAME_LEN || mn == NULL) {
+		AIM_FREE_IF_PTR(mn);
+		return PSU_TYPE_UNKNOWN;
+	}
+
+	if (!strncmp(mn, "DPS-2400AB-1", strlen("DPS-2400AB-1"))) {
+		if (modelname)
+			aim_strlcpy(modelname, mn, strlen("DPS-2400AB-1") < 
+				(modelname_len-1) ? (strlen("DPS-2400AB-1")+1) : 
+				(modelname_len-1));
+		AIM_FREE_IF_PTR(mn);
+		return PSU_TYPE_DELTA;
+	}
+
+	AIM_FREE_IF_PTR(mn);
+	return PSU_TYPE_UNKNOWN;
+}
+
+int psu_pmbus_info_get(int id, char *node, int *value)
+{
+	int  ret = 0;
+	char path[PSU_NODE_MAX_PATH_LEN] = {0};
+
+	*value = 0;
+
+	if (PSU1_ID == id) {
+		sprintf(path, "%s%s", PSU1_AC_PMBUS_PREFIX, node);
+	} else {
+		sprintf(path, "%s%s", PSU2_AC_PMBUS_PREFIX, node);
+	}
+
+	if (onlp_file_read_int(value, path) < 0) {
+		AIM_LOG_ERROR("Unable to read status from file(%s)\r\n", path);
+		return ONLP_STATUS_E_INTERNAL;
+	}
+
+	return ret;
+}
+
+int psu_ym2651y_pmbus_info_set(int id, char *node, int value)
+{
+	char path[PSU_NODE_MAX_PATH_LEN] = {0};
+
+	switch (id) {
+	case PSU1_ID:
+		sprintf(path, "%s%s", PSU1_AC_PMBUS_PREFIX, node);
+		break;
+	case PSU2_ID:
+		sprintf(path, "%s%s", PSU2_AC_PMBUS_PREFIX, node);
+		break;
+	default:
+		return ONLP_STATUS_E_UNSUPPORTED;
+	};
+
+	if (onlp_file_write_int(value, path) < 0) {
+		AIM_LOG_ERROR("Unable to write data to file (%s)\r\n", path);
+		return ONLP_STATUS_E_INTERNAL;
+	}
+
+	return ONLP_STATUS_OK;
+}
+
+
+
+#define PSU_SERIAL_NUMBER_LEN	18
+
+int psu_serial_number_get(int id, char *serial, int serial_len)
+{
+	int   size = 0;
+	int   ret  = ONLP_STATUS_OK;
+	char *prefix = NULL;
+
+	if (serial == NULL || serial_len < PSU_SERIAL_NUMBER_LEN)
+		return ONLP_STATUS_E_PARAM;
+
+	prefix = (id == PSU1_ID) ? PSU1_AC_PMBUS_PREFIX : PSU2_AC_PMBUS_PREFIX;
+
+	ret = onlp_file_read((uint8_t*)serial, PSU_SERIAL_NUMBER_LEN, &size,
+			     "%s%s", prefix, "psu_mfr_serial");
+
+	if (ret != ONLP_STATUS_OK || size != PSU_SERIAL_NUMBER_LEN)
+		return ONLP_STATUS_E_INTERNAL;
+
+
+	serial[PSU_SERIAL_NUMBER_LEN] = '\0';
+	return ONLP_STATUS_OK;
+}
+
+enum onlp_fan_dir onlp_get_fan_dir(void)
+{
+	return FAN_DIR_F2B;
+}
---
+++ packages/platforms/accton/x86-64/as9736-64d/platform-config/r0/src/python/x86_64_accton_as9736_64d_r0/__init__.py
@@ -0,0 +1,83 @@
+from onl.platform.base import *
+from onl.platform.accton import *
+
+class OnlPlatform_x86_64_accton_as9736_64d_r0(OnlPlatformAccton,
+                                              OnlPlatformPortConfig_64x400_2x10):
+
+    PLATFORM='x86-64-accton-as9736-64d-r0'
+    MODEL="AS9736-64D"
+    SYS_OBJECT_ID=".9736.64"
+
+    def baseconfig(self):
+        self.insmod('optoe')
+        #self.insmod('ym2651y')
+        self.insmod('accton_i2c_psu')
+        #os.system('insmod /lib/modules/5.4.40-OpenNetworkLinux/kernel/drivers/i2c/busses/i2c-ismt.ko')
+        os.system("modprobe i2c-ismt")
+
+        for m in [ 'sys', 'cpld', 'fan', 'psu', 'leds', 'fpga' ]:
+            self.insmod("x86-64-accton-as9736-64d-%s.ko" % m)
+
+        ########### initialize I2C bus 0 ###########
+        # initialize multiplexer (PCA9548)
+        self.new_i2c_device('pca9548', 0x72, 0)
+        # initiate multiplexer (PCA9548)
+        self.new_i2c_devices(
+            [
+                # initiate multiplexer (PCA9548)
+                ('pca9548', 0x71, 3),
+                ('pca9548', 0x71, 5),
+                ('pca9548', 0x76, 9),
+                ('pca9548', 0x70, 10),
+                ('pca9548', 0x70, 11),
+                ('pca9548', 0x70, 12),
+                ('pca9548', 0x70, 18),
+                ('pca9548', 0x70, 19),
+                ]
+            )
+
+        self.new_i2c_devices([
+            # initialize CPLD & FPGA
+            ('as9736_64d_sys_cpld', 0x60, 6),
+            ('sys_fpga', 0x60, 17),
+            ('as9736_64d_fan', 0x33, 25),
+            #('as9736_64d_pdb_l_cpld', 0x60, 36),
+            #('as9736_64d_pdb_r_cpld', 0x60, 44),
+            ('scm_cpld', 0x35, 51),
+            #('as9736_64d_udb_cpld1', 0x60, 60), read by pcie
+            #('as9736_64d_udb_cpld2', 0x61, 61), read by pcie
+            #('as9736_64d_ldb_cpld1', 0x61, 68), read by pcie
+            #('as9736_64d_ldb_cpld2', 0x62, 69), read by pcie
+            ])
+
+        self.new_i2c_devices([
+            # inititate TMP
+            ('lm75', 0x48, 2),
+            ('lm75', 0x49, 2),
+            ('lm75', 0x4C, 14),
+            ('lm75', 0x49, 27),
+            ('lm75', 0x48, 27),
+            ('lm75', 0x48, 34),
+            ('lm75', 0x49, 42),
+            ('lm75', 0x48, 57),
+            ('lm75', 0x4C, 58),
+            ('lm75', 0x4C, 65),
+            ('lm75', 0x4D, 66),
+         ])
+
+        self.new_i2c_devices([
+            # initiate PSU-1
+            ('as9736_64d_psu1', 0x50, 33),
+            ('acbel_fsh082', 0x58, 33),
+
+            # initiate PSU-2
+            ('as9736_64d_psu2', 0x51, 41),
+            ('acbel_fsh082', 0x59, 41),
+         ])
+
+        self.new_i2c_device('as973d_64d_sys', 0x51, 20)
+
+        # initialize pca9548 idle_state
+        subprocess.call('echo -2 | tee /sys/bus/i2c/drivers/pca954x/*-00*/idle_state > /dev/null', shell=True)
+
+        return True
