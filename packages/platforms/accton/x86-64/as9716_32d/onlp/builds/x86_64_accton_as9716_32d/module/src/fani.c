5540c552fe675aed73844f5ce175cecfa1b0c713
Willy Liu <willy@accton.com>
2025-08-27 16:39:31 +0800

    [ACCTON][AS9716-32D] Correct fan direction
    Signed-off-by: Willy Liu <willy@accton.com>

--- packages/platforms/accton/x86-64/as9716_32d/onlp/builds/x86_64_accton_as9716_32d/module/src/fani.c
+++ packages/platforms/accton/x86-64/as9716_32d/onlp/builds/x86_64_accton_as9716_32d/module/src/fani.c
@@ -129,7 +129,7 @@ _onlp_fani_info_get_fan(int fid, onlp_fan_info_t* info)
         return ONLP_STATUS_E_INTERNAL;
     }
 
-	info->status |= value ? ONLP_FAN_STATUS_F2B : ONLP_FAN_STATUS_B2F;
+	info->status |= value ? ONLP_FAN_STATUS_B2F : ONLP_FAN_STATUS_F2B;
 
 
     /* get front fan speed
